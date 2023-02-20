eaf233acf6211b630479a922c1512c0947e4a7c3
Sing <16409791+singyiu@users.noreply.github.com>
2023-02-19 17:46:43 -0800

    init

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import { Space, Button } from 'antd'
+
+const NavBar: FC = (props) => {
+  return (
+    <>
+      <Space wrap>
+        <Button>Projects</Button>
+      </Space>
+    </>
+  )
+}
+
+export default NavBar
