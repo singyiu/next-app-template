a33af6e6bda4c1309f682e51c81fe7cc14032628
Sing <16409791+singyiu@users.noreply.github.com>
2023-02-27 22:38:05 -0800

    src/components/web3/ContractReadStatistic.tsx

---
+++ src/components/web3/ContractReadStatistic.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react'
+import { Col, Row, Statistic, Card } from 'antd'
+import CountUp from 'react-countup'
+
+import { useHardhatContractRead } from '@/hooks/HardhatContracts'
+
+const countUpFormatter = (value: any) => (
+  <CountUp end={value} separator="," decimals={4} />
+)
+
+type Props = {
+  title: string
+  contractName: string
+  contractReadConfig: any
+  formatterEnabled: boolean
+  formatter?: (value: any) => any
+}
+const ContractReadStatistic: FC<Props> = ({
+  title,
+  contractName,
+  contractReadConfig,
+  formatterEnabled = true,
+  formatter = countUpFormatter,
+}) => {
+  const dataObj = useHardhatContractRead(contractName, contractReadConfig)
+
+  return (
+    <Row gutter={16}>
+      <Col span={24}>
+        <Card style={{ marginTop: 16 }}>
+          <Statistic
+            title={title}
+            value={dataObj?.data || 0}
+            formatter={formatterEnabled ? formatter : null}
+          />
+        </Card>
+      </Col>
+    </Row>
+  )
+}
+
+export default ContractReadStatistic
--- src/pages/debug/index.tsx
+++ src/pages/debug/index.tsx
@@ -1,13 +1,24 @@
 import _ from 'lodash'
+import { ethers } from 'ethers'
 
 import { useHardhatContracts } from '@/hooks/HardhatContracts'
 import Title from '@/components/typography/Title'
+import ContractReadStatistic from '@/components/web3/ContractReadStatistic'
 
 export default function Debug() {
   const contracts = useHardhatContracts()
   return (
     <>
       <Title>Contract names: {_.join(_.keys(contracts), ',')}</Title>
+      <ContractReadStatistic
+        title="beneficiary"
+        contractName="NpNFT"
+        contractReadConfig={{
+          functionName: 'beneficiary',
+          //select: (data) => ethers.utils.getAddress(data),
+        }}
+        formatterEnabled={false}
+      />
     </>
   )
 }
